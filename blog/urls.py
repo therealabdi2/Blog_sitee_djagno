9c7e85fe2819ea991010f4b0c965648a888c587c
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-07-09 23:40:06 +0500

    removed login

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("", login_required(views.StartingPageView.as_view()), name="starting_page"),
+    path("", views.StartingPageView.as_view(), name="starting_page"),
     path("posts", views.AllPostsView.as_view(), name="posts_page"),
     path("posts/<slug:slug>", views.PostDetailView.as_view(), name="posts_detail_page"),
     path("read-later", views.ReadLaterView.as_view(), name="read-later")
