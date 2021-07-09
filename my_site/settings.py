3a343c1edfe0e6259a25532220af164d5ad468b9
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-07-09 23:54:31 +0500

    added static

--- my_site/settings.py
+++ my_site/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
@@ -136,7 +137,7 @@ MEDIA_ROOT = BASE_DIR / "uploads"
 
 MEDIA_URL = "/files/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Configure Django App for Heroku.
 
 
